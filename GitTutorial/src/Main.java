f60faacce5625021fc5704a9711e1fcd0ba39920
Josh Aksamit <joshua.aksamit@gmail.com>
2024-05-10 15:51:30 -0700

    Version 13

--- GitTutorial/src/Main.java
+++ GitTutorial/src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         //This is code for the github tutorial
 
-        System.out.println("Version 12");
+        System.out.println("Version 13");
     }
 }
